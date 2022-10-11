11cc3abd530da9e5565fae264444ea59f42861b4
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2022-10-11 17:19:10 -0300

    adding game bases

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
+import boardgame.Board;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      Board board = new Board(8, 8);
     }
 }
