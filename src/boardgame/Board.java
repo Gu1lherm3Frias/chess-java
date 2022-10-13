c5669b950fbd2554f11e707d88b1b58b826f9c03
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2022-10-13 18:24:41 -0300

    a lot of things

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,7 +9,7 @@ public class Board {
     this.rows = rows;
     this.columns = columns;
     pieces = new Piece[rows][columns];
-    
+
   }
 
   public int getRows() {
@@ -28,5 +28,11 @@ public class Board {
     this.columns = columns;
   }
 
-  
+  public Piece piece(int row, int column) {
+    return pieces[row][column];
+  }
+
+  public Piece piece(Position position) {
+    return pieces[position.getRow()][position.getColumn()];
+  }
 }
